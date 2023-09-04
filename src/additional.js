df7bce675d318166039bf19de8bb6030e862f973
PVjacheslav <128468517+PVjacheslav@users.noreply.github.com>
2023-09-04 22:17:36 +0300

    change in task

--- src/additional.js
+++ src/additional.js
@@ -1,24 +1,25 @@
-import { galleryDiv } from './index';
 import axios from 'axios';
 const API_KEY = '39210182-6a28ff40429aa6ef86c4432d6';
-
 const limitPage = 40;
-
 axios.defaults.baseURL = 'https://pixabay.com/api/';
-
 const serchImg = async (querySerch, pageSerch) => {
-  const { data } = await axios({
-    params: {
-      key: API_KEY,
-      q: querySerch,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: 'true',
-      per_page: limitPage,
-      page: pageSerch,
-    },
-  });
-  return data;
+  try {
+    const { data } = await axios({
+      params: {
+        key: API_KEY,
+        q: querySerch,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        per_page: limitPage,
+        page: pageSerch,
+      },
+    });
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
 
 function renderImg(data) {
@@ -33,25 +34,25 @@ function renderImg(data) {
         comments,
         downloads,
       }) => {
-        `<a class="photo-link" href="${largeImageURL}">
-      <div class="photo-card">
-        <img src="${webformatURL}" alt="${tags}}" loading="lazy" />
-        <div class="info">
-          <p class="info-item">
-            <b>${likes}</b>
-          </p>
-          <p class="info-item">
-            <b>${views}</b>
-          </p>
-          <p class="info-item">
-            <b>${comments}</b>
-          </p>
-          <p class="info-item">
-            <b>${downloads}</b>
-          </p>
-        </div>
-      </div>
-    </a>`;
+        return `<a class="photo-link" href="${largeImageURL}">
+          <div class="photo-card">
+            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+              <p class="info-item">
+                <b>${likes}</b>
+              </p>
+              <p class="info-item">
+                <b>${views}</b>
+              </p>
+              <p class="info-item">
+                <b>${comments}</b>
+              </p>
+              <p class="info-item">
+                <b>${downloads}</b>
+              </p>
+            </div>
+          </div>
+        </a>`;
       }
     )
     .join('');
--- src/index.js
+++ src/index.js
@@ -1,7 +1,73 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import Notiflix, { Loading, Notify } from 'notiflix';
-import { limitPage, serchImg, renderImg } from './additional';
+
+import axios from 'axios';
+const API_KEY = '39210182-6a28ff40429aa6ef86c4432d6';
+const limitPage = 40;
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+const serchImg = async (querySerch, pageSerch) => {
+  try {
+    const response = await axios({
+      params: {
+        key: API_KEY,
+        q: querySerch,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        per_page: limitPage,
+        page: pageSerch,
+      },
+    });
+    if (response.data) {
+      return response.data;
+    } else {
+      throw new Error('Отримано порожню відповідь від сервера');
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+function renderImg(data) {
+  const markup = data.hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<a class="photo-link" href="${largeImageURL}">
+          <div class="photo-card">
+            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+              <p class="info-item">
+                <b>${likes}</b>
+              </p>
+              <p class="info-item">
+                <b>${views}</b>
+              </p>
+              <p class="info-item">
+                <b>${comments}</b>
+              </p>
+              <p class="info-item">
+                <b>${downloads}</b>
+              </p>
+            </div>
+          </div>
+        </a>`;
+      }
+    )
+    .join('');
+  galleryDiv.insertAdjacentHTML('beforeend', markup);
+}
 
 const elements = {
   serchForm: document.querySelector('.js-search-form'),
@@ -11,14 +77,12 @@ const elements = {
 };
 
 const galleryDiv = elements.divGallery;
-let searchQuery = null;
-let querySerch = '';
-let pageSerch = '';
 
-elements.serchForm.addEventListener('submit', onSubmitForm);
+let querySerch = '';
+let pageSerch = 1;
 const simpleBox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt', // отримати заголовок
-  captionDelay: 250, //затримка
+  captionsData: 'alt',
+  captionDelay: 250,
 });
 
 function onSubmitForm(e) {
@@ -28,11 +92,13 @@ function onSubmitForm(e) {
     return;
   }
   querySerch = query;
-  elements.divGallery.innerHTML = '';
+  galleryDiv.innerHTML = '';
+  pageSerch = 1;
   firstNumberImg(querySerch, pageSerch);
   elements.serchForm.reset();
 }
 
+elements.serchForm.addEventListener('submit', onSubmitForm);
 const firstNumberImg = async (query, pageSerch) => {
   try {
     Loading.circle('Loading', {
@@ -40,7 +106,7 @@ const firstNumberImg = async (query, pageSerch) => {
     });
     const data = await serchImg(query, pageSerch);
     Loading.remove();
-    if (!data.hits.lenght) {
+    if (!data.hits.length) {
       Notify.failure(
         'Failure is simply the opportunity to begin again, this time more intelligently.'
       );
@@ -49,31 +115,29 @@ const firstNumberImg = async (query, pageSerch) => {
     renderImg(data);
     simpleBox.refresh();
     if (pageSerch === 1) {
-      Notify.success(
-        'Do not try to become a person of success but try to become a person of value.'`We found ${data.totalHits} images.`
-      );
+      Notify.success(`We found ${data.totalHits} images.`);
     }
   } catch (error) {
-    // console.log(error);
+    console.error(error);
     Notify.failure('Oops! Try again!');
   }
 };
 
 function onLoadMore() {
-  page += 1;
+  pageSerch += 1;
   simpleBox.refresh();
-  firstNumberImg(query, page, limitPage)
+  firstNumberImg(querySerch, pageSerch)
     .then(data => {
       renderImg(data.hits);
       simpleBox.refresh();
       const totalPages = Math.ceil(data.totalHits / limitPage);
-      if (page > totalPages) {
+      if (pageSerch > totalPages) {
         Notify.failure(
           "We're sorry, but you've reached the end of the possible results"
         );
       }
     })
-    .catch(error => console.log(error));
+    .catch(error => console.error(error));
 }
 
 function checkEndPage() {
@@ -88,5 +152,3 @@ function loadMorePage() {
     onLoadMore();
   }
 }
-
-export { galleryDiv };
